903294e585ad2596f3874ed9bfb2a47e66a5ee10
bbplay110 <bbplay110@gmail.com>
2019-03-26 00:43:43 +0800

    20190326

--- Assets/Scripts/Scene/MovingPlatform.cs
+++ Assets/Scripts/Scene/MovingPlatform.cs
@@ -7,12 +7,14 @@ public class MovingPlatform : MonoBehaviour {
     public GameObject HitArea;
     public Transform point1;
     public Transform point2;
+    public iTween.EaseType easeType=iTween.EaseType.linear;
     public float MoveTime;
     public float WaitTime;
 	// Use this for initialization
 	void Start () {
         Invoke("MoveToPoint2", WaitTime);
-        HitArea.GetComponent<MeshRenderer>().enabled=false;
+        if(HitArea!=null)
+            HitArea.GetComponent<MeshRenderer>().enabled=false;
     }
 
     // Update is called once per frame
@@ -23,12 +25,12 @@ public class MovingPlatform : MonoBehaviour {
     private void MoveToPoint1()
     {
         Invoke("MoveToPoint2", WaitTime+MoveTime);
-        iTween.MoveTo(Platform, iTween.Hash("position",point1.position,"time",MoveTime));
+        iTween.MoveTo(Platform, iTween.Hash("position",point1.position,"time",MoveTime, "easeType",easeType));
         
     }
     private void MoveToPoint2()
     {
         Invoke("MoveToPoint1", WaitTime + MoveTime);
-        iTween.MoveTo(Platform, iTween.Hash("position", point2.position, "time", MoveTime));
+        iTween.MoveTo(Platform, iTween.Hash("position", point2.position, "time", MoveTime, "easeType", easeType));
     }
 }
