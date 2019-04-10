cbd3d4509aa9ceb3d9963c695f93e83e8e468ee8
bbplay110 <bbplay110@gmail.com>
2019-04-10 20:47:06 +0800

    Revert "20190410-2"
    This reverts commit f0052cb4ef0bad27bfdd80d5bf215775bb4ade38.

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -9,16 +9,8 @@ public class Boss : MonoBehaviour {
     private Animator BossAnimator;
     private Transform Player;
     public GameObject oraoraLeftHand,oraoraRightHand;
-    private Transform PlayerCamera;
-    private bool CanControllCamera;
-    private Vector3 tmpCameraPoint;
-    private float tmpCameraDistance;
-    // Use this for initialization
-    private void Awake()
-    {
-        PlayerCamera = GameObject.FindObjectOfType<Camera3rdControl>().target;
-    }
-    void Start () {
+	// Use this for initialization
+	void Start () {
         BossAnimator = GetComponent<Animator>();
         Player = GameObject.Find("Player").transform;
 	}
@@ -47,32 +39,11 @@ public class Boss : MonoBehaviour {
                      Player.position.z);
             this.transform.LookAt(targetPostition);
         }
-
-        CameraControll();
-    }
-    void CameraControll()
-    {
-        if (CanControllCamera)
-            PlayerCamera.position = ((Player.position + new Vector3(0, 2, 0)) + (transform.position + new Vector3(0, 50, 0))) / 2;
-            FindObjectOfType<Camera3rdControl>().distence = Vector3.Distance(Player.position + new Vector3(0, 2, 0), transform.position + new Vector3(0, 50, 0)) + 8;
+        
 
     }
     private void OnEnable()
     {
         HpSet.SetActive(true);
-        tmpCameraPoint = PlayerCamera.localPosition;
-        tmpCameraDistance = FindObjectOfType<Camera3rdControl>().distence;
-        FindObjectOfType<Camera3rdControl>().maxDistence = 300;
-        
-
-        CanControllCamera = true;
-    }
-    private void OnDisable()
-    {
-        if (tmpCameraPoint != null)
-            PlayerCamera.localPosition = tmpCameraPoint;
-        FindObjectOfType<Camera3rdControl>().maxDistence = 8;
-        FindObjectOfType<Camera3rdControl>().distence = tmpCameraDistance;
-
     }
 }
