5b237b1471804e5a7087ee320566833ea2d56899
bbplay110 <bbplay110@gmail.com>
2019-05-13 19:26:15 +0800

    20190513-2

---
+++ Assets/Scripts/sfxPlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class sfxPlay : MonoBehaviour {
+    public AudioClip[] sfx;
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<AudioSource>() == null)
+        {
+            gameObject.AddComponent<AudioSource>();
+
+            //audioSource.outputAudioMixerGroup=
+        }
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+	}
+	public void playSfx(int sfxToPlay)
+    {
+        audioSource.PlayOneShot(sfx[sfxToPlay]);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
