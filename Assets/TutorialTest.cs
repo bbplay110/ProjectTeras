297470d9957ad31388eadd77f284b3b20237cc2e
bbplay110 <bbplay110@gmail.com>
2019-05-22 18:42:50 +0800

    20190522-3

---
+++ Assets/TutorialTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTest : MonoBehaviour
+{
+
+    public GameObject TriggerZone;
+    bool KnowTutorial = false;
+
+
+    private void Awake()
+    {
+        if(KnowTutorial == true)
+        { 
+            TriggerZone.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            TriggerZone.SetActive(true);
+            KnowTutorial = true;
+        }
+    }
+
+    public void CloseTurorial()
+    {
+        if (KnowTutorial == true)
+        {
+            TriggerZone.SetActive(false);
+        }
+
+    }
+}
