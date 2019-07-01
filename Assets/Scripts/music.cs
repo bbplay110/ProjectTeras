ced29433a6361038e4a9ce41900811cea250d0ce
bbplay110 <bbplay110@gmail.com>
2019-07-01 11:23:08 +0800

    20190701

---
+++ Assets/Scripts/music.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour {
+    public AudioClip[] audios = new AudioClip[2];
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<AudioSource>().clip = audios[0];
+        this.GetComponent<AudioSource>().Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("B"))
+        {
+            
+        }
+    }
+}
