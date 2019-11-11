eb10b2bd42346260dc1a1c3ef30c7dd708cc1ef3
bbplay110 <bbplay110@gmail.com>
2019-11-11 17:27:44 +0800

    20191111

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class DeadZone : MonoBehaviour {
     public Transform SpawnPoint;
+    public bool hide = true;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        if (hide && GetComponent<MeshRenderer>() != null)
+            GetComponent<MeshRenderer>().enabled = false;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/Enemy/BossDemo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossDemo : MonoBehaviour {
+    
+    public bool turnWithPlayer;
+    public bool look=false;
+
+    private Animator BossAnimator;
+    public float rightHandIKWeight=0;
+    private Transform Player;
+    public GameObject IKAim;
+    private float lookWeight=0;
+    //--時停變數
+    private Animator Ani;
+
+    //--
+    // Use this for initialization
+    void Start () {
+        BossAnimator = GetComponent<Animator>();
+        Player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (look)
+        {
+            if (lookWeight < 1)
+            {
+                lookWeight += 1*Time.deltaTime;
+            }
+        }
+        if (!look && lookWeight > 0)
+        {
+
+            lookWeight -= 1 * Time.deltaTime;
+        }
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        if (look)
+        {
+            BossAnimator.SetLookAtWeight(lookWeight,lookWeight*0.5f,lookWeight,lookWeight);
+            BossAnimator.SetLookAtPosition(Player.position+new Vector3(0,15,0));
+        }
+        else if (!look)
+        {
+
+            BossAnimator.SetLookAtWeight(lookWeight, lookWeight * 0.5f,lookWeight,lookWeight);
+        }
+    }
+    public void loook()
+    {
+    }
+
+}
