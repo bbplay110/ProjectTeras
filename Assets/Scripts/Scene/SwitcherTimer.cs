cb9cff67f41db59d52feeb68daeacf173da27ebf
bbplay110 <bbplay110@gmail.com>
2019-11-14 12:51:42 +0800

    20191114

---
+++ Assets/Scripts/Scene/SwitcherTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitcherTimer : MonoBehaviour {
+    public GameObject[] switchObject;
+    public float time = 1;
+    private int a=0;
+	// Use this for initialization
+	void Start () {
+        foreach (var item in switchObject)
+        {
+            item.SetActive(false);
+        }
+        switchObject[0].SetActive(true);
+        InvokeRepeating("openObject",time,time);
+	}
+	void openObject()
+    {
+        switchObject[a % switchObject.Length].SetActive(false);
+        a += 1;
+        switchObject[a % switchObject.Length].SetActive(true);
+    }
+	// Update is called once per frame
+}
